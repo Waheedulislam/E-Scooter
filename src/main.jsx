92faa156b20eaeb5348d633db5fa172af245ca13
Whaeedil <baoppyhossen1234@gmail.com>
2024-09-09 02:12:58 +0600

    forget-pass

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 // import Home from './Pages/Home/Home.jsx'
 import router from './Components/Route/Route.jsx'
 import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </React.StrictMode>
 )
