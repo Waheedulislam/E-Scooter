95e0db8654d4b8aefbc8e06bfd4d3d4d4a5c8f2d
Whaeedil <baoppyhossen1234@gmail.com>
2024-09-08 18:29:13 +0600

    react-router

---
+++ src/Components/Route/Route.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../../Layouts/MainLayout";
+import Home from "../../Pages/Home/Home";
+import ProductListing from "../../Pages/Product Listing/ProductListing";
+import ProductDetails from "../../Pages/Product Details/ProductDetails";
+import Login from "../Loing-Register/Login";
+import Register from "../Loing-Register/Register";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/ProductListing',
+                element: <ProductListing />
+            },
+            {
+                path: '/ProductDetails',
+                element: <ProductDetails />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            },
+        ]
+    },
+]);
+
+export default router;
---
+++ src/Pages/Product Listing/ProductListing.jsx
@@ -0,0 +1,10 @@
+
+const ProductListing = () => {
+    return (
+        <div>
+            <h1>Product Listing page</h1>
+        </div>
+    );
+};
+
+export default ProductListing;
